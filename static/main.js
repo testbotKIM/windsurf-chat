6f19fb10740a840b72de6db851fb8868a580a95f
unknown <testbotKIM>
2025-04-19 17:03:50 +0900

    채팅창뒤뒤로가기버튼

--- static/main.js
+++ static/main.js
@@ -1,4 +1,13 @@
+function backToRoomList() {
+    document.getElementById('chat').style.display = 'none';
+    document.getElementById('roomListView').style.display = 'block';
+    document.getElementById('login').style.display = 'none';
+    // 필요하다면 소켓 연결 해제 등 추가 작업 가능
+}
+window.backToRoomList = backToRoomList;
+
 let socket;
+function createRoom() {
 function createRoom() {
     const nickname = document.getElementById('nickname').value;
     if (!nickname) {
@@ -30,6 +39,16 @@ function sendMsg() {
     if (msg) socket.emit('message', {msg});
     document.getElementById('msg').value = '';
 }
+// 메시지 입력창에서 Enter 키로 메시지 전송
+const msgInput = document.getElementById('msg');
+if (msgInput) {
+    msgInput.addEventListener('keydown', function(e) {
+        if (e.key === 'Enter') {
+            sendMsg();
+        }
+    });
+}
+
 function sendEmoji(emoji) {
     socket.emit('emoji', {emoji});
 }
@@ -44,7 +63,13 @@ function addMsgFromObj(obj, idx) {
     div.id = 'msg'+idx;
     div.innerHTML = `<b>${obj.nickname}</b> [${obj.timestamp}]: `;
     if (obj.type === 'file') {
-        div.innerHTML += `<a href="${obj.msg}" target="_blank">${obj.filename}</a>`;
+        // 이미지 확장자면 미리보기, 아니면 링크
+        const isImage = /\.(jpg|jpeg|png|gif|bmp|webp)$/i.test(obj.filename);
+        if (isImage) {
+            div.innerHTML += `<img src="${obj.msg}" alt="${obj.filename}" style="max-width:150px; max-height:150px; cursor:pointer; border:1px solid #ccc; margin:5px 0;" onclick="window.open('${obj.msg}','_blank')">`;
+        } else {
+            div.innerHTML += `<a href="${obj.msg}" target="_blank">${obj.filename}</a>`;
+        }
     } else {
         div.innerHTML += obj.msg;
     }
